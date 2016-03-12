5ad692b478d818185f330527e32fb3a6968dedec
Sam Sundar K <sam.sundar@misys.com>
2016-03-12 23:09:02 +0530

    CP initial Commit 2

---
+++ cashportal/src/main/java/com/svcet/cashportal/repository/CountryMasterRepository.java
@@ -0,0 +1,9 @@
+package com.svcet.cashportal.repository;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.svcet.cashportal.domain.CountryMaster;
+
+public interface CountryMasterRepository extends PagingAndSortingRepository<CountryMaster, Long> {
+
+}
